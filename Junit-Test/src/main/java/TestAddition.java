0330a1d363ee5d58bbe0fa95af73a2e4c54dcffa
d-satya <53555137+d-satya@users.noreply.github.com>
2019-08-27 12:34:16 +0530

    First Commit

---
+++ Junit-Test/src/main/java/TestAddition.java
@@ -0,0 +1,6 @@
+
+public class TestAddition {
+	public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ Junit-Test/src/test/java/TestAdditionTest.java
@@ -0,0 +1,21 @@
+import junit.framework.TestCase;
+
+public class TestAdditionTest extends TestCase {
+	public void testAddingTwoPositiveIntegers() throws Exception {
+        int expected = 11;
+        int actual = new TestAddition().add(4, 7);
+        assertEquals(expected, actual);
+    }
+  
+    public void testAddingTwoNegativeIntegers() throws Exception {
+        int expected = -11;
+        int actual = new TestAddition().add(-4, -7);
+        assertEquals(expected, actual);
+    }
+  
+    public void testAddingNegativeIntegerToPositiveInteger() throws Exception {
+        int expected = -3;
+        int actual = new TestAddition().add(4, -7);
+        assertEquals(expected, actual);
+    }
+}
