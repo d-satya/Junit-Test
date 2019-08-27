dcf961090fb8df16f2483c084ab60dca1ad26163
d-satya <53555137+d-satya@users.noreply.github.com>
2019-08-27 13:23:23 +0530

    4th commit

--- Junit-Test/src/test/java/TestAdditionTest.java
+++ Junit-Test/src/test/java/TestAdditionTest.java
@@ -14,8 +14,13 @@ public class TestAdditionTest extends TestCase {
     }
   
     public void testAddingNegativeIntegerToPositiveInteger() throws Exception {
-        int expected = -5;
+        int expected = -3;
         int actual = new TestAddition().add(4, -7);
+        assertEquals(expected, actual);
+    }
+    public void addingtwobignumbers() throws Exception {
+        int expected = 150;
+        int actual = new TestAddition().add(30, 120);
         assertEquals(expected, actual);
     }
 }
